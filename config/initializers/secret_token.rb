1107b51fa263e7fb0288c792c0d87bb36e314222
Patrick Turley [pturley] <patrick.turley@gmail.com>
2014-08-07 22:48:53 -0500

    basic app with nothing in it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Selfiequest::Application.config.secret_key_base = '008b095abc289c0ee26234e41e1338a9ac5a272866ba981149e71c845d3a7e3f00b4e9e84b935d388936d5ad92e0fc0d939c0db360d7b6f684733c2c814c4e55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Selfiequest::Application.config.session_store :cookie_store, key: '_selfiequest_session'
