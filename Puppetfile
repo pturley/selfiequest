ef5c3723c5f9533cf3595265ef93e851992356fa
Patrick Turley [pturley] <patrick.turley@gmail.com>
2014-08-12 13:04:39 -0500

    [#1] Adding basic vagrant setup

---
+++ Puppetfile
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+forge "https://forgeapi.puppetlabs.com"
+
+mod "jdowning/rbenv", "1.3.0"
+mod "puppetlabs/postgresql", "3.4.2"
---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+exec { 'apt-update':
+  command => "/usr/bin/apt-get update",
+  onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
+}
+
+Exec["apt-update"] -> Package <| |>
+
+package { ["libpq-dev", "sqlite3", "libsqlite3-dev"]: ensure => present }
+
+class { 'rbenv':
+  install_dir => '/opt/rbenv',
+  latest      => true
+}
+
+rbenv::plugin { 'sstephenson/ruby-build': }
+rbenv::build { '2.1.1': global => true }
+
+file { "/home/vagrant/bundle" :
+  ensure => directory,
+  owner => "vagrant",
+  group => "vagrant"
+}
+
+class { 'postgresql::server':
+  postgres_password => 'password',
+}
